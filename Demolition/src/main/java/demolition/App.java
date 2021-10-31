7a770fdf41383e1328ff90fc44894419057b8177
tangjas20 <tangjas2020@gmail.com>
2021-10-31 11:29:19 +1100

    Cleaning up

--- Demolition/src/main/java/demolition/App.java
+++ Demolition/src/main/java/demolition/App.java
@@ -14,35 +14,28 @@ public class App extends PApplet {
     public static final int HEIGHT = 480;
 
     public static final int FPS = 60;
-    Board board = new Board();
     private Player player;
-    private GoalTile GoalImage;
-    private SolidWall SolidImage;
-    private EmptyWall EmptyImage;
-    private BrokenWall BrokenImage;
     private redEnemy redEnemy;
     private yellowEnemy yellowEnemy;
     private String[][] currentBoard = new String[13][15];
     private Integer currentTimer = 0;
-    private List<SolidWall> solidWalls = new ArrayList<SolidWall>();
-    private List<BrokenWall> brokenWalls = new ArrayList<BrokenWall>();
-    private List<EmptyWall> emptyWalls = new ArrayList<EmptyWall>();
-    private List<GoalTile> goalTile = new ArrayList<GoalTile>();
     List<PImage[]> yellowEnemyImageList = new ArrayList<PImage[]>();
     List<PImage[]> redEnemyImageList = new ArrayList<PImage[]>();
     List<PImage[]> imageList = new ArrayList<PImage[]>();
     private Map<String, String> pathTimeMap = new HashMap<String, String>();
-    Boolean redEnemyTF = false;
-    Boolean yellowEnemyTF = false;
+    Boolean hasRedEnemy = false;
+    Boolean hasYellowEnemy = false;
     private List<String> boardArrayName = new ArrayList<String>();
     private List<Integer> boardArrayTime = new ArrayList<Integer>();
     private int boardCounter = 0;
     private int lives;
     private timer timerIcon;
     PFont font;
-
+    boolean keyReleased = true;
+    Board board = new Board();
     public App() {
         //construct objects here
+        player = board.getPlayer();
     }
 
     public void settings() {
@@ -51,6 +44,7 @@ public class App extends PApplet {
 
     public void setup() {
         frameRate(FPS);
+
         // Load images during setup
         //Board
         font = createFont("PressStart2P-regular.ttf", 16);
@@ -69,108 +63,55 @@ public class App extends PApplet {
             boardArrayName.add(key);
         }
 
-        currentBoard = board.makeBoard(boardArrayName.get(boardCounter));
+        currentBoard = board.makeBoard(boardArrayName.get(boardCounter), this);
         currentTimer =  boardArrayTime.get(boardCounter);
 
-        map(currentBoard);
-    }
-    
-    public void map(String[][] mapBoard){
-        redEnemyTF = false;
-        yellowEnemyTF = false;
-        solidWalls.clear();
-        emptyWalls.clear();
-        brokenWalls.clear();
-        goalTile.clear();
-        this.timerIcon = new timer(180, this);
-        for(int i = 0; i < mapBoard.length; i++){
-            for(int j = 0; j < mapBoard[0].length; j++){
-                int x = j;
-                int y = i;
-                String mapTile = mapBoard[i][j];
-                if(mapTile.equals("P")){
-                    this.player = new Player(x, y, this);
-                }
-
-                if(mapTile.equals("Y")){
-                    this.yellowEnemy = new yellowEnemy(x, y, this);
-                    yellowEnemyTF = true;
-                }
-
-                if(mapTile.equals("R")){
-                    this.redEnemy = new redEnemy(x, y, this);
-                    redEnemyTF = true;
-                }
-
-                if(mapTile.equals("W")){
-
-                    this.SolidImage = new SolidWall(x, y, this);
-                    solidWalls.add(SolidImage);
-                }
-                else if(mapTile.equals("B")){
-                    //Broken
-                    this.BrokenImage = new BrokenWall(x, y, this);
-                    brokenWalls.add(BrokenImage);
-                }
-                else if(mapTile.equals(" ") || mapTile.equals("P") || mapTile.equals("R") || mapTile.equals("Y")){
-                    //EmptyTile
-                    this.EmptyImage = new EmptyWall(x, y, this);
-                    emptyWalls.add(EmptyImage);
-                }
-                else if(mapTile.equals("G")){
-                    //GoalTile
-                    this.GoalImage = new GoalTile(x, y, this);
-                    goalTile.add(GoalImage);
-                }
-            }
-        }
+        board.map(currentBoard, this);
     }
 
     public void draw() {
         //Main loop here
         if(frameCount % 60 == 1){
             background(255, 128, 0);
-            this.timerIcon.draw(this);
-            this.timerIcon.tick();
-            text(timerIcon.getTimer(), 350, 40);
-            text(lives, 145, 40);
+           // this.timerIcon.draw(this);
+            //this.timerIcon.tick();
+            //text(timerIcon.getTimer(), 350, 40);
+            //text(lives, 145, 40);
         }
         if(frameCount % 12 == 1){
 
-            for(SolidWall i: solidWalls){
-                this.SolidImage = i;
-                this.SolidImage.draw(this);
+            for(SolidWall solidWall: board.getSolidWallsList()){
+                solidWall.draw(this);
             }
-            for(BrokenWall i: brokenWalls){
-                this.BrokenImage = i;
-                this.BrokenImage.draw(this);
+            for(BrokenWall brokenWall: board.getBrokenWallsList()){
+                brokenWall.draw(this);
             }
-            for(EmptyWall i: emptyWalls){
-                this.EmptyImage = i;
-                this.EmptyImage.draw(this);
+            for(EmptyWall emptyWall: board.getEmptyWallsList()){
+                emptyWall.draw(this);
             }
-            for(GoalTile i: goalTile){
-                this.GoalImage = i;
-                this.GoalImage.draw(this);
+            for(GoalTile goalImage: board.getGoalTileList()){
+                goalImage.draw(this);
             }
-            if(redEnemyTF == true){
+            if(hasRedEnemy == true){
                 this.redEnemy.tick();
                 this.redEnemy.draw(this);
             }
-            if(yellowEnemyTF == true){
+            if(hasYellowEnemy == true){
                 this.yellowEnemy.tick();
                 this.yellowEnemy.draw(this);
             }   
             this.player.tick();
             this.player.draw(this);
 
+//Change this into a for loop to check or every GoalImage in the GoalTile Array
+            for(GoalTile i: board.getGoalTileList()){
+                if(this.player.getX() == i.getX() && this.player.getY() == i.getY()){
 
-            if(this.player.getX() == GoalImage.getX() && this.player.getY() == GoalImage.getY()){
-
-                boardCounter ++;
-                currentBoard = board.makeBoard(boardArrayName.get(boardCounter));
-                currentTimer =  boardArrayTime.get(boardCounter);
-                map(currentBoard);
+                    boardCounter ++;
+                    currentBoard = board.makeBoard(boardArrayName.get(boardCounter), this);
+                    currentTimer =  boardArrayTime.get(boardCounter);
+                    board.map(currentBoard, this);
+                }
             }
             
         }
@@ -184,7 +125,7 @@ public class App extends PApplet {
             .get(3) corresponds with left player
         */
         if (key == CODED) {
-            if (keyCode == DOWN) {
+            if (keyCode == DOWN && keyReleased == true) {
                 player.changeOrientation(0);
                 if((currentBoard[player.getY()+1][player.getX()].equals(" ")) || (currentBoard[player.getY()+1][player.getX()].equals(" "))){
                     currentBoard[player.getY()][player.getX()] = " ";
@@ -192,7 +133,7 @@ public class App extends PApplet {
                     currentBoard[player.getY()][player.getX()] = "P";
                 }
 
-            } else if (keyCode == UP) {
+            } else if (keyCode == UP && keyReleased == true) {
                 player.changeOrientation(1);
                 if((currentBoard[player.getY()-1][player.getX()].equals(" ")) || (currentBoard[player.getY()-1][player.getX()].equals("G"))){
                     currentBoard[player.getY()][player.getX()] = " ";
@@ -200,7 +141,7 @@ public class App extends PApplet {
                     currentBoard[player.getY()][player.getX()] = "P";
                 }
 
-            } else if (keyCode == RIGHT) {
+            } else if (keyCode == RIGHT && keyReleased == true) {
                 player.changeOrientation(2);
                 if((currentBoard[player.getY()][player.getX()+1].equals(" ")) || (currentBoard[player.getY()][player.getX()+1].equals("G"))){
                     currentBoard[player.getY()][player.getX()] = " ";
@@ -208,7 +149,7 @@ public class App extends PApplet {
                     currentBoard[player.getY()][player.getX()] = "P";
                 }
 
-            } else if (keyCode == LEFT) {
+            } else if (keyCode == LEFT && keyReleased == true) {
                 player.changeOrientation(3);
                 if((currentBoard[player.getY()][player.getX()-1].equals(" ")) || (currentBoard[player.getY()][player.getX()-1].equals(" "))){
                     currentBoard[player.getY()][player.getX()] = " ";
@@ -217,9 +158,12 @@ public class App extends PApplet {
                 }
             }
         }
+        keyReleased = false;
     }
 
-    
+    public void keyReleased() {
+        this.keyReleased = true;
+    }
     public static void main(String[] args) {
         PApplet.main("demolition.App");
     }
--- Demolition/src/main/java/demolition/Board.java
+++ Demolition/src/main/java/demolition/Board.java
@@ -4,14 +4,28 @@ import java.util.ArrayList;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.List;
+import processing.core.PApplet;
 
 public class Board {
     
+    private List<SolidWall> solidWalls = new ArrayList<SolidWall>();
+    private List<BrokenWall> brokenWalls = new ArrayList<BrokenWall>();
+    private List<EmptyWall> emptyWalls = new ArrayList<EmptyWall>();
+    private List<GoalTile> goalTile = new ArrayList<GoalTile>();
+    boolean redEnemyTF;
+    boolean yellowEnemyTF;
+    private timer timerIcon;
+    private redEnemy redEnemy;
+    private yellowEnemy yellowEnemy;
+    private Player player;
+
     String[][] madeBoard = new String[13][15];
+
     public Board(){
     }
 
-    public String[][] makeBoard(String fileName) {
+    public String[][] makeBoard(String fileName, PApplet app) {
 
         try{
             File firstFile = new File(fileName);
@@ -30,23 +44,77 @@ public class Board {
         return madeBoard;
     }
 
+    public void map(String[][] mapBoard, PApplet app){
+        redEnemyTF = false;
+        yellowEnemyTF = false;
+        solidWalls.clear();
+        emptyWalls.clear();
+        brokenWalls.clear();
+        goalTile.clear();
+        this.timerIcon = new timer(180, app);
+        for(int i = 0; i < mapBoard.length; i++){
+            for(int j = 0; j < mapBoard[0].length; j++){
+                int x = j;
+                int y = i;
+                String mapTile = mapBoard[i][j];
+                if(mapTile.equals("P")){
+                    this.player = new Player(x, y, app);
+                }
 
-/*
-    public void get1stBoard() {
-        for(int i = 0; i < firstBoard.length; i++){
-            if(i >= 1)
-                System.out.print("\n");
-            for(int j = 0; j < firstBoard[0].length; j++){
-                System.out.print(firstBoard[i][j]);
+                if(mapTile.equals("Y")){
+                    this.yellowEnemy = new yellowEnemy(x, y, app);
+                    yellowEnemyTF = true;
+                }
+
+                if(mapTile.equals("R")){
+                    this.redEnemy = new redEnemy(x, y, app);
+                    redEnemyTF = true;
+                }
+
+                if(mapTile.equals("W")){
+
+                    SolidWall SolidImage = new SolidWall(x, y, app);
+                    solidWalls.add(SolidImage);
+                }
+                else if(mapTile.equals("B")){
+                    //Broken
+                    BrokenWall BrokenImage = new BrokenWall(x, y, app);
+                    brokenWalls.add(BrokenImage);
+                }
+                else if(mapTile.equals(" ") || mapTile.equals("P") || mapTile.equals("R") || mapTile.equals("Y")){
+                    //EmptyTile
+                    EmptyWall EmptyImage = new EmptyWall(x, y, app);
+                    emptyWalls.add(EmptyImage);
+                }
+                else if(mapTile.equals("G")){
+                    //GoalTile
+                    GoalTile GoalImage = new GoalTile(x, y, app);
+                    goalTile.add(GoalImage);
+                }
             }
         }
-    }*/
-
-    public String[][] get2ndBoard() {
-        return null;
+    }
+    
+    public List<SolidWall> getSolidWallsList(){
+        return this.solidWalls;
     }
 
-    
+    public List<BrokenWall> getBrokenWallsList(){
+        return this.brokenWalls;
+    }
+
+    public List<EmptyWall> getEmptyWallsList(){
+        return this.emptyWalls;
+    }
+
+    public List<GoalTile> getGoalTileList(){
+        return this.goalTile;
+    }
+
+    public Player getPlayer(){
+        return this.player;
+    }
+
 
     public static void main(String[] args){
         /*Board board = new Board();
