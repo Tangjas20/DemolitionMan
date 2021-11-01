60130df3a413bacfe3e53f31ff75bef01ffa345e
Tangjas20 <tangjas2020@gmail.com>
2021-11-01 22:56:57 +1100

    Bomb added with optimisations
    Added the Bomb
    Added playerMovement interface
    Optimised App.java with draw having 60fps without framecount divisions

--- Demolition/src/main/java/demolition/Bomb.java
+++ Demolition/src/main/java/demolition/Bomb.java
@@ -17,6 +17,9 @@ public class Bomb extends GameObject {
     private int timerSprite;
     private int currentSprite;
     private boolean isAlive;
+    boolean killedPlayer = false;
+    boolean killedYellowEnemy = false;
+    boolean killedRedEnemy = false;
 
     public Bomb(int x, int y, PApplet app, String[][] currentBoard){
         super(x, y, app);
@@ -51,68 +54,92 @@ public class Bomb extends GameObject {
     public void setCanExplodePath(){
         if(this.currentBoard[y][x+1].equals("W")){
             ;
-        }
+        }//Right
         else if(this.currentBoard[y][x+1].matches(" |R|Y|P")){
-            explodablePath[0] ++;
-            if(!this.currentBoard[y][x+2].equals("W")){
-                explodablePath[0] ++;
+            explodablePath[3] ++;
+            if(this.currentBoard[y][x+2].matches(" |R|Y|P|B")){
+                explodablePath[3] ++;
             }
         }
+        else if(this.currentBoard[y][x+1].equals("B")){
+                explodablePath[3] += 3;
+            }
 
+
+        //Left
         if(this.currentBoard[y][x-1].equals("W")){
             ;
         }
         else if(this.currentBoard[y][x-1].matches(" |R|Y|P")){
-            explodablePath[3] ++;
-            if(!this.currentBoard[y][x-2].equals("W")){
-                explodablePath[3] ++;
+            explodablePath[0] ++;
+            if(this.currentBoard[y][x-2].matches(" |R|Y|P|B")){
+                explodablePath[0] ++;
             }
         }
-
+        else if(this.currentBoard[y][x-1].equals("B")){
+                explodablePath[0] += 3;
+            }
+        //DOWN
         if(this.currentBoard[y+1][x].equals("W")){
             ;
         }
         else if(this.currentBoard[y+1][x].matches(" |R|Y|P")){
             explodablePath[2] ++;
-            if(!this.currentBoard[y+2][x].equals("W")){
+            if(this.currentBoard[y+2][x].matches(" |R|Y|P|B")){
                 explodablePath[2] ++;
             }
         }
-
+        else if(this.currentBoard[y+1][x].equals("B")){
+                explodablePath[2] += 3;
+            }
+        //UP
         if(this.currentBoard[y-1][x].equals("W")){
             ;
         }
         else if(this.currentBoard[y-1][x].matches(" |R|Y|P")){
             explodablePath[1] ++;
-            if(!this.currentBoard[y-2][x].equals("W")){
+            if(this.currentBoard[y-2][x].matches(" |R|Y|P|B")){
                 explodablePath[1] ++;
             }
         }
+        else if(this.currentBoard[y-1][x].equals("B")){
+                explodablePath[1] += 3;
+            }
         
         this.explodablePath = explodablePath;
 
     }
-    public void drawExplosion(PApplet app) {
-        app.image(explosionSprite[0], this.x*32, this.y*32);
-        if (explodablePath[0] == 1) app.image(explosionSprite[3], this.x*32, this.y*32-32);
-        else if (explodablePath[0] == 2) {
-            app.image(explosionSprite[2], this.x*32, this.y*32-32);
-            app.image(explosionSprite[3], this.x*32, this.y*32-64);
+    /*
+        this.explosionSprite[0] = app.loadImage("src/main/resources/explosion/centre.png");
+        this.explosionSprite[1] = app.loadImage("src/main/resources/explosion/horizontal.png");
+        this.explosionSprite[2] = app.loadImage("src/main/resources/explosion/vertical.png");
+        this.explosionSprite[3] = app.loadImage("src/main/resources/explosion/end_top.png");
+        this.explosionSprite[4] = app.loadImage("src/main/resources/explosion/end_right.png");
+        this.explosionSprite[5] = app.loadImage("src/main/resources/explosion/end_bottom.png");
+        this.explosionSprite[6] = app.loadImage("src/main/resources/explosion/end_left.png");
+        */
+    public void drawExplosion(PApplet app) {//Left
+        app.image(explosionSprite[0], this.x*32, this.y*32+64);
+        if (explodablePath[0] == 1 || explodablePath[0] == 3) app.image(explosionSprite[6], this.x*32-32, this.y*32+64); //Left Close
+        else if (explodablePath[0] == 2) {//Left Far
+            app.image(explosionSprite[1], this.x*32-32, this.y*32+64);
+            app.image(explosionSprite[6], this.x*32-64, this.y*32+64);
         }
-        if (explodablePath[1] == 1) app.image(explosionSprite[4], this.x*32+32, this.y*32);
+        //UP
+        if (explodablePath[1] == 1 || explodablePath[1] == 3) app.image(explosionSprite[3], this.x*32, this.y*32+64-32);
         else if (explodablePath[1] == 2) {
-            app.image(explosionSprite[1], this.x*32+32, this.y*32);
-            app.image(explosionSprite[4], this.x*32+64, this.y*32);
-        }
-        if (explodablePath[2] == 1) app.image(explosionSprite[5], this.x*32, this.y*32+32);
+            app.image(explosionSprite[2], this.x*32, this.y*32+64-32);
+            app.image(explosionSprite[3], this.x*32, this.y*32+64-64);
+        }//DOWN
+        if (explodablePath[2] == 1 || explodablePath[2] == 3) app.image(explosionSprite[5], this.x*32, this.y*32+64+32);
         else if (explodablePath[2] == 2) {
-            app.image(explosionSprite[2], this.x*32, this.y*32+32);
-            app.image(explosionSprite[5], this.x*32, this.y*32+64);
-        }
-        if (explodablePath[3] == 1) app.image(explosionSprite[6], this.x*32-32, this.y*32);
+            app.image(explosionSprite[2], this.x*32, this.y*32+32+64);
+            app.image(explosionSprite[5], this.x*32, this.y*32+64+64);
+        }//RIGHT
+        if (explodablePath[3] == 1 || explodablePath[3] == 3) app.image(explosionSprite[4], this.x*32+32, this.y*32+64);
         else if (explodablePath[3] == 2) {
-            app.image(explosionSprite[1], this.x*32-32, this.y*32);
-            app.image(explosionSprite[6], this.x*32-64, this.y*32);
+            app.image(explosionSprite[1], this.x*32+32, this.y*32+64);
+            app.image(explosionSprite[4], this.x*32+64, this.y*32+64);
         }
         timerSprite += 1;
         if (timerSprite == 30) {
@@ -121,7 +148,7 @@ public class Bomb extends GameObject {
     }
 
     public void draw(PApplet app) {
-        app.image(bombSprite[currentSprite], this.x*32, this.y*32);
+        app.image(bombSprite[currentSprite], this.x*32, this.y*32+64);
         timerSprite += 1;
 
         if(timerSprite == 15) {
@@ -137,29 +164,52 @@ public class Bomb extends GameObject {
     }
 
     public void explosion() {
+        //Left
       if (explode(this.x-1, this.y, currentBoard)) explode(this.x-2, this.y, currentBoard);
+      //Down
       if (explode(this.x, this.y+1, currentBoard)) explode(this.x, this.y+2, currentBoard);
+      //Right
       if (explode(this.x+1, this.y, currentBoard)) explode(this.x+2, this.y, currentBoard);
+      //Up
       if (explode(this.x, this.y-1, currentBoard)) explode(this.x, this.y-2, currentBoard);
     }
 
     public boolean explode(int row, int column, String[][] currentBoard) {
         String tile = currentBoard[column][row];
 
-        if (tile == " ") {
+        if (tile.equals(" ")) {
           return true;
-        } else if (tile == "B") {
-            currentBoard[column][row] = " ";
+        } else if (tile.equals("B")) {
+            this.currentBoard[column][row] = " ";
+            return false;
+        } else if (tile.equals("P")) {
+            killedPlayer = true;
+            
             return true;
-        } else if (tile == "P") {
+        } else if (tile.equals("Y")) {
+            this.currentBoard[column][row] = " ";
+            killedYellowEnemy = true;
             return true;
-        } else if (tile == "Y") {
-            return true;
-        } else if (tile == "R") {
+        } else if (tile.equals("R")) {
+            this.currentBoard[column][row] = " ";
+            killedRedEnemy = true;
             return true;
         }
         return false;
 
     }
+
+    public boolean getIsAlive(){
+        return this.isAlive;
+    }
+
+    public boolean getExplosionFinished(){
+        return this.explosionFinished;
+    }
+
+    public String[][] getBoard(){
+        return this.currentBoard;
+    }
+
 
 }
---
+++ Demolition/src/main/java/demolition/playerMovement.java
@@ -0,0 +1,16 @@
+package demolition;
+
+interface playerMovement{
+
+    void movementLeft();
+
+    void movementRight();
+
+    void movementUP();
+
+    void movementDOWN();
+
+    void changeOrientation(int orientation);
+
+
+}
